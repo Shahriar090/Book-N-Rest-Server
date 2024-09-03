893ab151a64736f90f5fa6d23380d57d6ddab481
Shahriar <shahriarh655@gmail.com>
2024-09-03 20:40:42 +0600

    Implement custom ApiError class with TypeScript

---
+++ src/utils/apiError.ts
@@ -0,0 +1,38 @@
+type TApiError = {
+  statusCode: number;
+  data: any;
+  message: string;
+  success: boolean;
+  errors: any[];
+  stack: string;
+};
+
+class ApiError extends Error implements TApiError {
+  statusCode: number;
+  data: any;
+  message: string;
+  success: boolean;
+  errors: any[];
+  stack: string;
+
+  constructor(
+    statusCode: number,
+    message: string = "Something Went Wrong",
+    errors: any[] = [],
+    stack: string = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+    this.stack = stack || "";
+
+    if (!stack) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
