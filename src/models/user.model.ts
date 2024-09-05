d0a4533af0b17ec5c7b14d6ea42551212d895461
Shahriar <shahriarh655@gmail.com>
2024-09-05 15:28:17 +0600

    Implemented access and refresh token generation functionality with custom methods in the user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcryptjs";
-import jwt from "json-web-token";
+import jwt from "jsonwebtoken";
 
 export type UserType = {
   _id: string;
@@ -48,9 +48,39 @@ userSchema.pre("save", async function (next) {
 });
 
 // comparing password between given and stored
+//injecting custom methods
 
 userSchema.methods.isPasswordCorrect = async function (password: string) {
   return await bcrypt.compare(password, this.password);
+};
+
+// generating access and refresh token with injecting custom methods.
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      firstName: this.firstName,
+      lastName: this.lastName,
+    },
+    process.env.ACCESS_TOKEN_SECRET as string,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+// refresh token
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET as string,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 };
 
 export const User = mongoose.model<UserType>("User", userSchema);
