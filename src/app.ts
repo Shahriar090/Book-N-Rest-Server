8896347c6cbbc04012a3981d577a445156ed906a
Shahriar <shahriarh655@gmail.com>
2024-09-22 21:13:41 +0600

    Implemented cloudinary configuration

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import userRouter from "./routes/user.routes";
 
 export const app = express();
 
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded());
