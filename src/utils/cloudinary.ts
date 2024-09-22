71e290384c390098d1b8c1c6775f73c05f8710ca
Shahriar <shahriarh655@gmail.com>
2024-09-22 21:50:35 +0600

    Implemented a utility function to handle uploading file on cloudinary

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
@@ -6,3 +6,29 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+export const uploadOnCloudinary = async (
+  localFilePath: string
+): Promise<string | UploadApiResponse> => {
+  try {
+    if (!localFilePath) {
+      return "No Local File Found";
+    }
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File Uploaded On Cloudinary", response.url);
+    fs.unlink(localFilePath, (err) => {
+      if (err) {
+        console.log("Error Deleting Local File", err);
+      } else {
+        console.log("Local File Deleted Successfully");
+      }
+    });
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return "File Uploading Failed";
+  }
+};
